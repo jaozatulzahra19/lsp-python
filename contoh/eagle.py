6d4f4b070bd7be55b624dba8d9e8dcb139f35096
Mila3010 <milakarmeliyah45@gmail.com>
2022-05-26 14:43:04 +0700

    burung rajawali

---
+++ contoh/eagle.py
@@ -0,0 +1,9 @@
+from flying_bird import FlyingBird
+
+class Eagle(FlyingBird):
+    
+    def Eagle(FlyingBird):
+        
+        def fly(self):
+            print("Eagle fly to the sky")
+            
